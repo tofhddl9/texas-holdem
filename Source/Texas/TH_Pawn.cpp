75744b6d0401faa6efd0d97b8cd73f7997c5f9a6
DESKTOP-Hun <tofhddl9@gmail.com>
2017-08-30 01:44:37 +0900

    Call() is called 2 times in a turn

--- Source/Texas/TH_Pawn.cpp
+++ Source/Texas/TH_Pawn.cpp
@@ -21,8 +21,8 @@ ATH_Pawn::ATH_Pawn()
 void ATH_Pawn::BeginPlay()
 {
 	Super::BeginPlay();
-	acted = false;
 	is_tick_start = false;
+	acted = false;
 	tick = 0;
 }
 
@@ -32,10 +32,12 @@ void ATH_Pawn::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 	if (is_tick_start && tick < 200) tick++;
-	if (tick == 200) {
+	if (is_tick_start&&tick == 200) {
+		tick = 0;
 		Call();
 		is_tick_start = false;
-		tick = 0;
+		//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, "!!!!");
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, FString::Printf(TEXT("%lld"), (long long*)this));
 	}
 
 	{
@@ -78,6 +80,9 @@ void ATH_Pawn::Call()
 {
 	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, "Call");
 	acted = true;
+	ATH_GameState* const MyGameState = GetWorld() != NULL ? GetWorld()->GetGameState<ATH_GameState>() : NULL;
+	MyGameState->SetNumPlayerActed(MyGameState->GetNumPlayerActed()+1);
+	MyGameState->SetPot(MyGameState->GetPot() + 1);
 }
 
 void ATH_Pawn::Fold()
@@ -107,17 +112,7 @@ void ATH_Pawn::SetActed(bool act)
 {
 	acted = act;
 }
-/*
-void ATH_Pawn::ActivateInput()
-{
-	EnableInput(Cast<APlayerController>(Controller));
-}
 
-void ATH_Pawn::DeactivateInput()
-{
-	DisableInput(Cast<APlayerController>(Controller));
-}
-*/
 APlayerController* ATH_Pawn::GetPlayerController(APlayerState* player_state_)
 {
 	for (FConstPlayerControllerIterator it = player_state_->GetWorld()->GetPlayerControllerIterator(); it; ++it)
